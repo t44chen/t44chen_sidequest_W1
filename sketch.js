442472588e00ff5023930888f86f86edf18bec1b
Tracey Chen <t44chen@uwaterloo.ca>
2026-01-12 19:23:48 -0500

    Make canvas responsive and enhance draw function
    Updated canvas size to be responsive and added text elements.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER, CENTER);
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(0, 0, 255);
+  background(245);
+
+  noStroke();
+  fill(222, 184, 135);
+  rect(width / 2, height / 2, 320, 100);
+
+  fill(101, 67, 33);
+  textSize(80);
+  textStyle(BOLD);
+  text("JENGA", width / 2, height / 2 + 10);
+
+  if (frameCount % 60 < 30) {
+    fill(150);
+    textSize(20);
+    textStyle(NORMAL);
+    text("- INSERT COIN -", width / 2, height / 2 + 80);
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
